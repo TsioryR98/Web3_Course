00ac5a03622ec527a320104a2b535198c0f6f684
TsioryR98 <hei.tsiory.2@gmail.com>
2025-02-20 09:29:26 +0100

    feat: add edit action for resource post and update in userdataProvider

--- src/post/postDataProvider.ts
+++ src/post/postDataProvider.ts
@@ -5,6 +5,8 @@ import {
   GetListParams,
   GetListResult,
   GetOneParams,
+  UpdateParams,
+  UpdateResult,
   GetOneResult,
   Identifier,
   RaRecord,
@@ -32,7 +34,7 @@ export const postDataProvider: DataProvider = {
       const { page = 1, perPage = 10 } = pagination || {}; //if undefined then empty obj
       const offset = (page - 1) * perPage; //page 2 then 11-20 offset 10
 
-      const postData = await data.json() as PostRecord[];
+      const postData = (await data.json()) as PostRecord[];
       const pageNumber = Math.ceil(postData.length / perPage);
 
       //Sort postdata
@@ -63,42 +65,77 @@ export const postDataProvider: DataProvider = {
 
   getOne: async function <RecordType extends RaRecord = PostRecord>(
     resource: string,
-    params: GetOneParams<RecordType> & QueryFunctionContext
-  ): Promise<GetOneResult<RecordType>> { //RecordType for getting the data from db
+    params: GetOneParams<RecordType> & QueryFunctionContext,
+  ): Promise<GetOneResult<RecordType>> {
+    //RecordType for getting the data from db
     try {
       const { id } = params;
-      const data = await fetch(`https://jsonplaceholder.typicode.com/posts/${id}`, {
-        method: `GET`
-      });
+      const data = await fetch(
+        `https://jsonplaceholder.typicode.com/posts/${id}`,
+        {
+          method: `GET`,
+        },
+      );
       const result: GetOneResult = {
-        data: await data.json() as PostRecord
-      }
+        data: (await data.json()) as PostRecord,
+      };
       return result;
     } catch (error) {
       throw error;
     }
-
   },
-  create: async function <RecordType extends Omit<RaRecord, 'id'> = PostRecord,
-    ResultRecordType extends RaRecord = RecordType & { id: Identifier }>(
-      resource: string,
-      params: CreateParams
-    ): Promise<CreateResult<ResultRecordType>> { //ResultRecordType for save data
+  create: async function <
+    RecordType extends Omit<RaRecord, "id"> = PostRecord,
+    ResultRecordType extends RaRecord = RecordType & { id: Identifier },
+  >(
+    resource: string,
+    params: CreateParams,
+  ): Promise<CreateResult<ResultRecordType>> {
+    //ResultRecordType for save data
     try {
       const { data } = params;
-      const createdPost = await fetch(`https://jsonplaceholder.typicode.com/posts`,
+      const createdPost = await fetch(
+        `https://jsonplaceholder.typicode.com/posts`,
         {
           method: `POST`,
           headers: {
-            'Content-Type': 'application/json',
+            "Content-Type": "application/json",
           },
           body: JSON.stringify(data),
-        }
+        },
       );
-      const responseData = await createdPost.json() as PostRecord;
+      const responseData = (await createdPost.json()) as PostRecord;
       const result: CreateResult = {
         data: responseData as PostRecord,
       };
+      return result;
+    } catch (error) {
+      throw error;
+    }
+  },
+  update: async function <
+    RecordType extends Omit<RaRecord, "id"> = PostRecord,
+    ResultRecordType extends RaRecord = RecordType & { id: Identifier },
+  >(
+    resource: string,
+    params: UpdateParams,
+  ): Promise<UpdateResult<ResultRecordType>> {
+    try {
+      const { data, id } = params;
+      const updatePost = await fetch(
+        `https://jsonplaceholder.typicode.com/posts/${id}`,
+        {
+          method: "PUT",
+          headers: {
+            "Content-Type": "application/json",
+          },
+          body: JSON.stringify(data),
+        },
+      );
+      const responseData = await updatePost.json() as PostRecord;
+      const result: UpdateResult = {
+        data: responseData as PostRecord;
+      }
       return result;
     } catch (error) {
       throw error;
